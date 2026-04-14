195917dc49cd5350f38cafff2dd9835c669f6ecc
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-14 17:41:48 +0900

    [feat] 정보 조회 로직 구현

--- src/main/java/com/App.java
+++ src/main/java/com/App.java
@@ -18,7 +18,7 @@ public class App {
         Warrior warrior = new Warrior(name);
         System.out.printf("닉네임 설정 완료! %s\n",warrior.getName());
         while(true) {
-            System.out.println("종료, 전투 중 선택하여주세요.");
+            System.out.println("종료, 전투, 정보 조회 중 선택하여주세요.");
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
             switch (cmd) {
@@ -29,6 +29,17 @@ public class App {
                 case "전투" -> {
                     BattleManager bm = new BattleManager(warrior, new Slime(), scanner);
                     bm.start();
+                }
+                case "정보 조회" -> {
+                    System.out.println("정보");
+                    System.out.println("------------------");
+                    System.out.println("닉네임 : " + warrior.getName());
+                    System.out.println("레벨 : " + warrior.getLevel());
+                    System.out.printf("체력 : %d\n", warrior.getHp());
+                    System.out.printf("공격력 : %d\n", warrior.getAttackPower());
+                    System.out.printf("방어력 : %d\n", warrior.getDefensePower());
+                    System.out.printf("경험치 : %d / %d\n", warrior.getCurExp(), warrior.getExp());
+                    System.out.println("------------------");
                 }
                 default -> System.out.println("다시 입력해주세요.");
             }
