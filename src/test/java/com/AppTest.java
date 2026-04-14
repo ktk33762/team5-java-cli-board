e8f21aa30eebbb47165af12f9e0db430cbada441
Rakhyunn <krh0323@pukyong.ac.kr>
2026-04-14 17:32:09 +0900

    [test] 전투 도망, 정보 조회 테스트 코드 추가

--- src/test/java/com/AppTest.java
+++ src/test/java/com/AppTest.java
@@ -48,4 +48,34 @@ public class AppTest {
         bm.start();
         assertThat(warrior.getDied()).isTrue();
     }
+
+    @Test
+    @DisplayName("슬라임과 전투 : 도망")
+    void t4() {
+        String rs = AppTestRunner.run("""
+                용사
+                전투
+                2
+                종료
+                """);
+        assertThat(rs)
+                .contains("야생에서 슬라임 (이)가 나타났다!")
+                .contains("어떻게 하시겠습니까?(숫자 입력)")
+                .contains("1.전투 2.도망")
+                .contains("잽싸게 도망쳤습니다.");
+    }
+
+    @Test
+    @DisplayName("정보 조회")
+    void t5() {
+        String rs = AppTestRunner.run("""
+                용사
+                정보 조회
+                종료
+                """);
+        assertThat(rs)
+                .contains("닉네임 : 용사")
+                .contains("레벨 : 1")
+                .contains("체력 : 30");
+    }
 }
