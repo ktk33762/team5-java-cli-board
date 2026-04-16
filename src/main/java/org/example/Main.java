c21de9d927f779c0b53c4fda9e1691b1b9b63dc4
HeungJunBag <p2005hihi@gmail.com>
2026-04-16 16:34:14 +0900

    feat: Main 진입점 연결

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     static void main() {
+        App app = new App(
+                System.in,
+                System.out,
+                new ArticleService(new ArticleRepository())
+        );
+        app.run();
     }
 }
 
